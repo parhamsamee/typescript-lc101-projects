5ed592a10a9a2eacce77fb38ba31806697ea548c
Samee Hall <sameehall@MacBook-Pro.attlocal.net>
2020-12-03 19:34:58 -0600

    updates

---
+++ studio/Rocket.ts
@@ -0,0 +1,50 @@
+import { Astronaut } from './Astronaut';
+import { Cargo } from './Cargo';
+import { Payload } from './Payload';
+
+export class Rocket {
+    name: string;
+    totalCapacityKg: number;
+    cargoItems: Cargo[];
+    astronauts: Astronaut[];
+    constructor(name: string, totalCapacityKg: number) {
+        this.name = name;
+        this.totalCapacityKg = totalCapacityKg;
+        this.cargoItems = [];
+        this.astronauts = [];
+    };
+    sumMass (items: Payload[]): number {
+        let sum = 0;
+        for (let i = 0; i < items.length; i++) {
+            sum += items[i].massKg; 
+        }
+        return sum;
+    }
+    currentMassKg(): number {
+        return this.sumMass(this.cargoItems) + this.sumMass(this.astronauts);
+    }
+    canAdd(item: Payload): boolean {
+        if (this.currentMassKg() + item.massKg <= this.totalCapacityKg) {
+            return true;
+        }
+    }
+    addCargo(cargo: Cargo): boolean {
+        if (this.canAdd(cargo) === true) {
+            this.cargoItems.push(cargo);
+            return true;
+        } else {
+            return false;
+        }
+    }
+    addAstronaut(astronaut: Astronaut): boolean {
+        if (this.canAdd(astronaut) === true) {
+            this.astronauts.push(astronaut);
+            return true; 
+        } else {
+            return false;
+        }
+    }
+
+
+
+}
